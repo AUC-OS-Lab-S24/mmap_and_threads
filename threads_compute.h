fc0cafdec1a3af7653e85fa130935383f94094c2
omarelfouly <omarelfouly@aucegypt.edu>
2024-04-14 22:37:31 +0200

    Finished but multiply is brocken

--- threads_compute.h
+++ threads_compute.h
@@ -22,8 +22,97 @@ struct thread_args
     unsigned long (*func)(unsigned long, unsigned long);
 };
 
-unsigned long threads_compute(int num_threads, char *file_path, unsigned long (*func)(unsigned long, unsigned long))
+void *compute(void *arg)
 {
+    struct thread_args *args = (struct thread_args *)arg;
+    unsigned long *numbers = args->numbers;
+    unsigned long start = args->start;
+    unsigned long end = args->end;
+    unsigned long (*func)(unsigned long, unsigned long) = args->func;
+
+    unsigned long result = numbers[start];
+    for (int i = start + 1; i < end; i++)
+    {
+        result = func(result, numbers[i]);
+    }
+
+    return (void *)result;
+}
+
+unsigned long threads_compute(int num_threads, char *path, unsigned long (*func)(unsigned long, unsigned long))
+{
+    // Open the file
+    FILE *file = fopen(path, "r");
+
+    if (file == NULL)
+    {
+        printf("Error opening file!\n");
+        return 1;
+    }
+
+    // get the number of integers in the file
+    int temp, count = 1;
+    fscanf(file, "%d", &temp);
+
+    while (fscanf(file, ",%d", &temp) == 1)
+    {
+        count++;
+    }
+    // move file pointer to start
+    fseek(file, 0, SEEK_SET);
+
+    // Read the integers into an array
+    unsigned long *numbers = (unsigned long *)malloc(count * sizeof(unsigned long));
+    fscanf(file, "%lu", &numbers[0]);
+    for (int i = 1; i < count; i++)
+    {
+        fscanf(file, ",%lu", &numbers[i]);
+    }
+
+    // Close the file
+    fclose(file);
+
+    int size = count / num_threads;
+
+    // case where count is less than n_proc
+    if (size == 0)
+    {
+        size = 1;
+        num_threads = count;
+    }
+
+    // Create threads
+    pthread_t threads[num_threads];
+    struct thread_args args[num_threads];
+
+    for (int i = 0; i < num_threads; i++)
+    {
+        args[i].numbers = numbers;
+        args[i].start = i * size;
+        args[i].end = (i + 1) * size;
+        args[i].func = func;
+
+        if (i == num_threads - 1)
+        {
+            args[i].end = count;
+        }
+
+        pthread_create(&threads[i], NULL, compute, &args[i]);
+    }
+
+    // Join threads
+    unsigned long result = 0;
+    for (int i = 0; i < num_threads; i++)
+    {
+        unsigned long temp;
+        pthread_join(threads[i], (void **)&temp);
+        result = func(result, temp);
+    }
+
+    // Free the array
+    free(numbers);
+
+    return result;
 }
 
 #endif
