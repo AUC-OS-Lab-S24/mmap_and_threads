44c6039034a6acdc2c74e27340afd879ac473895
Ahmed-Waseem77 <ahmedwaseemr@aucegypt.edu>
2024-04-15 20:45:11 +0200

    better constants

--- main.c
+++ main.c
@@ -2,16 +2,16 @@
 #include "test_increasing_parallelism.h"
 #include <stdio.h>
 
-#define MYCORES 4
-#define MAX_PARALLELISM 2
-#define BIG_N   7420
+#define MYCORES 12
+#define MAX_PARALLELISM 50
+#define BIG_N   137420
 
 int main(int argc, char *argv[])
 {
     for (int i = 0; i < 5; i++)
     {
         test_increasing_parallelism(MAX_PARALLELISM , BIG_N, argc, argv, i);
-        test_increasing_N(100, 10000, 1000, MYCORES - 2, argc, argv, i);
+        test_increasing_N(1000, 1000000, 1000, MYCORES - 2, argc, argv, i);
     }
 
     return 0;
